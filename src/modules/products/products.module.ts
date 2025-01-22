948364a8a8401ee3423cd1f044f80fd3406a40bc
rodrigo <javierluis15975@hotmail.com>
2025-01-22 04:09:42 -0500

    feat: add RabbitMQ integration with configuration for exchanges and queues

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -9,7 +9,8 @@ import { Coupon, SchemaCoupon } from '../coupon/entities/coupon.entity'
 import { Product, SchemaProduct } from './entities/product.entity'
 import { ProductsController } from './products.controller'
 import { ProductsService } from './products.service'
-
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
+import { configExchange, configQueue } from './common/config-rabbit'
 @Module({
   imports: [
     CouponModule,
@@ -18,6 +19,22 @@ import { ProductsService } from './products.service'
       isGlobal: true,
       cache: false,
       envFilePath: '.env',
+    }),
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('RABBITMQ_URL'),
+        exchanges: configExchange,
+        queues: configQueue,
+        // connectionInitOptions: {
+        //   wait: false,
+        // },
+        // channels: {
+        //   default: {
+        //     // El prefetchCount en RabbitMQ controla la cantidad de mensajes que un consumidor puede recibir y procesar simultáneamente antes de enviar un ack (confirmación) de los mensajes procesados. Esto forma parte del mecanismo de QoS (Quality of Service) en RabbitMQ.
+        //     prefetchCount: 10,
+        //   },
+        // },
+      }),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
